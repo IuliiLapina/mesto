6cfc3f188ee5a978c14edc47a576999808733cb9
Iuliia Lapina <uncle-glasha7@yandex.ru>
2021-01-20 23:42:27 +0300

    feat: добавить интерактивность попапа

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,30 @@ let content = document.querySelector('.content');
 let editBtn = content.querySelector('.form__edit-button');
 let popup = content.querySelector('.popup');
 let popupCloseBtn = content.querySelector('.popup__close-btn');
+let nameProfile = content.querySelector('.profile__title');
+let nameInput = popup.querySelector('.input__text_type_name');
+let jobProfile = content.querySelector('.profile__subtitle');
+let jobInput = popup.querySelector('.input__text_type_job');
+let formElement = popup.querySelector(".input");
 
-let profileName = content.querySelector('.profile__title');
-let inputName = popup.querySelector('.input__text_type_name');
-inputName.value = profileName.textContent;
+//Добавим в поля попапа значения со страницы
+nameInput.value = nameProfile.textContent;
+jobInput.value = jobProfile.textContent;
 
-let profileJob = content.querySelector('.profile__subtitle');
-let inputJob = popup.querySelector('.input__text_type_job');
-inputJob.value = profileJob.textContent;
-
+//Добавим функцию открытия и закрытия попапа
 function popupToggle() {
   popup.classList.toggle('popup_opened');
 }
 
 editBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
+
+//Редактирование имени и информации о себе
+  function formSubmitHandler (evt) {
+    evt.preventDefault();
+    jobProfile.textContent = jobInput.value;
+    nameProfile.textContent = nameInput.value;
+    popupToggle();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
